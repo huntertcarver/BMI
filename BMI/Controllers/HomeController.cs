2247e5ff86fd2ecb967bf8844aff4d13198c00fe
Hunter <hunter@1968bird.com>
2022-02-28 15:33:17 -0600

    BMI calc ASP.NET MVC application

---
+++ BMI/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BMI.Models;
+
+namespace BMI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult getData()
+        {
+            userViewModel model = new userViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult displayBMI(string f, string l, string g, int m, int d, int y, int w, int h)
+        {
+            userViewModel model = new userViewModel(f,l,g,m,d,y,h,w);
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("getData", model);
+            }
+            DateTime bd = new DateTime((int)model.y, (int)model.m, (int)model.d);
+            model.age = DateTime.Now - bd;
+            model.bmi = (int)((model.w / Math.Pow(h, 2)) * 703);
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult chart()
+        {
+            return View();
+        }
+    }
+}
---
+++ BMI/Models/userViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace BMI.Models
+{
+    public class userViewModel
+    {
+        public userViewModel()
+        {
+
+        }
+        public userViewModel(string f, string l, string g, int m, int d, int y, int h, int w)
+        {
+            this.f = f;
+            this.l = l;
+            this.g = g;
+            this.m = m;
+            this.d = d;
+            this.y = y;
+            this.h = h;
+            this.w = w;
+        }
+
+        [Required]
+        [MaxLength(20)]
+        public string f, l;
+        [Required]
+        public string g;
+        [Required]
+        [Range(1,12)]
+        public int? m;
+        [Required]
+        [Range(1,31)]
+        public int? d;
+        [Required]
+        [Range(1900,2022)]
+        public int? y;
+        [Required]
+        [Range(48,108)]
+        public int? h;
+        [Range(50,500)]
+        [Required]
+        public int? w;
+
+        //calc
+        //years
+        public TimeSpan age;
+        public int hr;
+        public int bmi;
+    }
+}
